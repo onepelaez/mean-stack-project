0a75a945943e7734230154c9c0d15a5c62d1d160
Juan Pablo Pelaez <onepelaez.sar@gmail.com>
2017-08-22 22:10:44 -0500

    MEAN Stack setup

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
     uri: 'mongodb://localhost:27017/' + this.db,
-    secret: 'crypto',
+    secret: crypto,
     db: 'mean-angular-2'
 }
